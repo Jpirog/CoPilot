562ac64d8025d77a564bb7af9f1d6f6b6396892e
John Pirog <jpirog@hotmail.com>
2021-09-26 18:53:14 -0500

    nav fixes, 404 page

---
+++ client/components/PageNotFound.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PageNotFound = () => {
+    return (
+        <div id="content-wrapper">
+          <div id="wrapper404">
+              <img src="https://i.imgur.com/qIufhof.png" />
+              <div id="info404">
+                  <h3>This page could not be found</h3>
+                  <h3>Please visit our home page to use the site</h3>
+              </div>
+          </div >
+        </div >
+    )
+}
+
+export default PageNotFound
